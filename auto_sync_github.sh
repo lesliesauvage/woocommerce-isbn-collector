fb81598c5255ea76cbdfcc13fe5917794bea92c6
lesliesauvage <sauvageleslie1@gmail.com>
2025-07-12 15:44:33 +0200

    Auto-sync: changements détectés

--- auto_sync_github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "[START: auto_sync_github.sh] $(date +%Y-%m-%d\ %H:%M:%S)" >&2
-
-# auto_sync_github.sh - Synchronisation automatique silencieuse
-
-cd /var/www/scripts-home-root/isbn/
-./sync_to_github.sh "Sync automatique $(date '+%Y-%m-%d %H:%M')" > /dev/null 2>&1
-
-echo "[END: auto_sync_github.sh] $(date +%Y-%m-%d\ %H:%M:%S)" >&2
